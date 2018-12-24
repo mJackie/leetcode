48bcd7522e919e4d8e11de1d2389b47659d42fdb
liu13 <1099976891@qq.com>
2018-12-24 11:43:31 +0800

    first commit

---
+++ code/lc5.java
@@ -0,0 +1,31 @@
+package code;
+/*
+ * 5. Longest Palindromic Substring
+ * 题意：找出给定字符串中最长的回文串
+ * 难度：Medium
+ * 分类：String, Dynamic Programming
+ * Tips：从后往前遍历，保证后续dp时，子情况已计算出
+ */
+public class lc5 {
+    public static void main(String[] args) {
+        String s = "cbbd";
+        System.out.println(longestPalindrome(s));
+    }
+
+    public static String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        String res = "";
+        for (int i = n-1; i>=0 ; i--) {
+            for (int j = i; j <n ; j++) {
+                if(s.charAt(i)==s.charAt(j)){
+                    if((j-i)<3 || dp[i+1][j-1])
+                        dp[i][j] = true;
+                    if(dp[i][j] && j-i+1>res.length())
+                        res = s.substring(i,j+1);   // 起始索引，终止索引(不包括，所以+1)
+                }
+            }
+        }
+        return res;
+    }
+}

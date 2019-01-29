f7fb3a417105cc6d76c0d1b24bc6e0afcd348ffc
liu13 <1099976891@qq.com>
2019-01-29 21:59:06 +0800

    20190129

---
+++ code/lc118.java
@@ -0,0 +1,11 @@
+package code;
+
+import java.util.List;
+
+public class lc118 {
+    public List<List<Integer>> generate(int numRows) {
+        while(numRows>0){
+            
+        }
+    }
+}

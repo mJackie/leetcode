028e14056f39577bc9c231bdf5cc3886eafd4562
liu13 <1099976891@qq.com>
2019-03-01 21:08:28 +0800

    20190301

--- code/lc295.java
+++ code/lc295.java
@@ -1,7 +1,15 @@
 package code;
 
 import java.util.PriorityQueue;
-
+/*
+ * 295. Find Median from Data Stream
+ * 题意：流数据中找中位数
+ * 难度：Hard
+ * 分类：
+ * 思路：用两个优先队列，一个保存左半边最大值，一个保存右半边最小值
+ *      保持左半边最多比右半边多一个数
+ * Tips：
+ */
 public class lc295 {
     class MedianFinder {
         PriorityQueue<Integer> pq1;    //默认是最小，右半边

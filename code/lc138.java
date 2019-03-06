d3e07b15a67c3a6fdd1c0a8b958e0c0bd7b6d0e9
liu13 <1099976891@qq.com>
2019-03-06 09:54:58 +0800

    20190306

--- code/lc138.java
+++ code/lc138.java
@@ -6,7 +6,7 @@ package code;
  * 分类：Hash Table, LinkedList
  * 思路：一种用HashTable，把源节点和复制的节点对应起来，空间复杂度为O(n)
  *      把要复制的节点直接连接在该节点之后，操作完以后再分开
- * Tips：和139做对比，因为需要每步骤返回值，所以递归的方法在这题更合适
+ * Tips：因为需要每步骤返回值，所以递归的方法在这题更合适
  */
 public class lc138 {
     class RandomListNode {

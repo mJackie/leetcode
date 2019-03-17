9e2990cdc8fed05c2ca44f537df82ca8e864ed54
liu13 <1099976891@qq.com>
2019-03-17 15:00:04 +0800

    20190317

--- code/lc33.java
+++ code/lc33.java
@@ -6,7 +6,8 @@ package code;
  * 难度：Medium
  * 分类：Array, Binary Search
  * 思路：二分查找的思路，多了一步判断，判断哪部分有序，是否在这部分中
- * Tips：注意边界判断，是否有等号
+ * Tips：注意边界判断，是否有等号。挺麻烦的，没有输入样本，很难想通边边角角
+ *       lc81
  */
 public class lc33 {
     public static void main(String[] args) {
@@ -36,6 +37,28 @@ public class lc33 {
                     start = mid+1;
                 else
                     end =mid-1;
+            }
+        }
+        return -1;
+    }
+
+    public int search2(int[] nums, int target) {
+        int begin = 0, end = nums.length-1;
+        while(begin<=end){
+            int mid = (begin+end)/2;
+            if(target==nums[mid]) return mid;
+            if(nums[begin]<=nums[mid]){  //左边有序，别忘了=，begin==end
+                if(target>=nums[begin]&&target<nums[mid]){
+                    end = mid-1;
+                }else{
+                    begin = mid+1;
+                }
+            }else{  //右边有序
+                if(target>nums[mid]&&target<=nums[end]){ //别忘了=
+                    begin = mid+1;
+                }else{
+                    end = mid-1;
+                }
             }
         }
         return -1;
---
+++ code/lc81.java
@@ -0,0 +1,36 @@
+package code;
+/*
+ * 31. Search in Rotated Sorted Array II
+ * 题意：在翻转有序数组中查找指定数，数组中可能有相等的元素
+ * 难度：Medium
+ * 分类：Array, Binary Search
+ * 思路：多加了一种情况，就是 nums[mid]==nums[begin]==nums[end]
+ * Tips：注意边界判断，是否有等号
+ *       lc33
+ */
+public class lc81 {
+    public boolean search(int[] nums, int target) {
+        int begin = 0, end = nums.length-1;
+        while(begin<=end){
+            int mid = (begin+end)/2;
+            if(target==nums[mid]) return true;
+            if(nums[begin]==nums[end]&& nums[end]==nums[mid]) end--;
+            else if(nums[begin]<=nums[mid]){  //左边有序
+                if(target>=nums[begin]&&target<nums[mid]){
+                    end = mid-1;
+                }else{
+                    begin = mid+1;
+                }
+            }else if(nums[mid]<nums[end]){  //右边有序
+                if(target>nums[mid]&&target<=nums[end]){
+                    begin = mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }else{
+                end--;
+            }
+        }
+        return false;
+    }
+}
